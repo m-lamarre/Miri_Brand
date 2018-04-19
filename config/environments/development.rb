6d462c6a15d64e147e2c3ee376ceb636cd5718d6
m-lamarre <studium.square.pantz@gmail.com>
2018-04-19 17:50:27 +0200

    added mockups file

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -25,6 +25,8 @@ Rails.application.configure do
 
     config.cache_store = :null_store
   end
+  config.assets.cache_store = :null_store
+  config.sass.cache
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
